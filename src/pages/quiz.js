55eaba2f9b8bfca923b3d5f4b023c99057f69320
Rory S Moeller <moellerr@oregonstate.edu>
2022-02-24 15:47:17 -0800

    Basic playlist parsing

--- src/pages/quiz.js
+++ src/pages/quiz.js
@@ -1,7 +1,32 @@
+import { useSpotifyPlaylist } from '../hooks/useSpotifyPlaylist';
 
+class TrackList {
+    constructor(playlist) {
+        this.tracks = []
+        playlist.map(entry => {
+            this.tracks.push(entry.track)
+        })
+        this.tracks.filter(track => track.type === 'track')
+    }
+}
+
+function parsePlaylistToTrackList(playlist) {
+    return new TrackList(playlist)
+}
 
 export function Quiz(props) {
+    const playlist_id = "51f1c2c97d53416b"
+    const [playlist, loading, error] = useSpotifyPlaylist(playlist_id, props.auth_token);
+    const tracklist= parsePlaylistToTrackList(playlist)
+    // console.log("===TrackList===", tracklist);
     return (
-        <div>Wow this is a quiz page</div>
+        <div>
+            Wow this is a quiz page
+            <div>
+                {loading && <p>Loading...</p>}
+                {error && <p>Error!</p>}
+                {!loading && !error && <p>{tracklist.tracks.map(track => track.name + " on " + track.album.name + " by " + track.artists[0].name)}</p>}
+            </div>
+        </div>
     )
 }
