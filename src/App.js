18464136cd6e1fcf02749bf2de77bc3ede989636
Rory S Moeller <moellerr@oregonstate.edu>
2022-02-28 09:30:19 -0800

    Set up global styles

--- src/App.js
+++ src/App.js
@@ -1,3 +1,7 @@
+/** @jsxImportSource @emotion/react */
+import { css, Global } from "@emotion/react";
+
+
 import logo from './logo.svg';
 import './App.css';
 
@@ -6,10 +10,17 @@ import { Navigate, Route, Routes, useSearchParams } from 'react-router-dom';
 import { Home } from './pages/home';
 import { Quiz } from './pages/quiz';
 
+
 function App(props) {
+    const globalStyles = css`
+        html {
+            height: 100%;
+        }
+    `
     console.log(props.auth_token)
     return (
         <div className="App">
+            <Global styles={globalStyles} />
             <Routes>
                 <Route path="/home" element={<Home auth_token={props.auth_token} />} />
                 <Route path="/quiz" element={<Quiz auth_token={props.auth_token}/>} />
