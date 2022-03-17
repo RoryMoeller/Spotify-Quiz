ff295fae7c9140d070a9868e8134838da9bd798c
Steven <nguyens5@oregonstate.edu>
2022-03-16 20:00:54 -0700

    style done screen

--- src/pages/done.js
+++ src/pages/done.js
@@ -1,9 +1,17 @@
+/** @jsxImportSource @emotion/react */
 
+import { css } from "@emotion/react";
+import colors from '../components/colorTheme';
 
 export function Done(props) {
-
+    const doneStyle = css`
+    padding-top: 150px;
+    padding-bottom: 150px;
+    color: rgb(${colors.standard.text.primary});
+    font-size: 40px;
+    `
     return (
-        <div>
+        <div css={doneStyle}>
             You finished the quiz wow! Your final score was { ((props.correctCount / props.questionCount) * 100).toFixed(1) }%
         </div>
     )
