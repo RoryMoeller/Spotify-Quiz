5fe61abcfac03c676b12eff9924e994f68ec6354
Steven <nguyens5@oregonstate.edu>
2022-03-16 20:20:51 -0700

    made title navlink for home for now

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,7 +2,9 @@
 
 import { css } from "@emotion/react";
 import colors from "./colorTheme"
-
+import { 
+    NavLink,
+  }from 'react-router-dom';
 export function Header(props) {
     const headerStyle = css`
         color: white;
@@ -15,10 +17,16 @@ export function Header(props) {
         background-color: rgba(${colors.standard.accents.primary}, .7);
         backdrop-filter: blur(5px);4
     `;
+    const otherStyle = css`
+    text-decoration: none;
+    color: white;
+`;
     return (
-        <div css={headerStyle}>
-            Spotify Playlist Quiz
-            {props.content && <div>{props.content}</div>}
+        <div>
+            <div css={headerStyle}>
+                <NavLink css={otherStyle} to="/home">Spotify Playlist Quiz</NavLink>
+                {props.content && <div>{props.content}</div>}
+            </div>
         </div>
     )
 }
