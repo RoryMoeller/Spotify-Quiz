22129b6d6c55eb1bb8a69941bfef1c593570dc61
Rory S Moeller <moellerr@oregonstate.edu>
2022-03-16 19:52:57 -0700

    position footer items better

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,13 +7,15 @@ export function Footer(props) {
     const footerStyle = css`
         display: flex;
         align-items: center;
-        justify-content: space-around;
-        
+        justify-content: space-between;
+        padding-left: 40px;
+        padding-right: 40px;
+        font-size: 20px;
         height: 60px;
         padding-top: 5px;
         position: fixed;
         bottom: 0px;
-        width: 100%;
+        width: calc(100vw - 80px);
         box-shadow: 0px -7px 10px 5px rgba(0,0,0,0.2);
         background-color: rgba(${colors.standard.accents.primary}, .7);
         backdrop-filter: blur(5px);4
